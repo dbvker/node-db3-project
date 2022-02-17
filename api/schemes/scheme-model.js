11ca607d8c4cb889b754f2b07f27a7817ca34f2b
Dylan Baker <dylanmbaker@yahoo.com>
2022-02-16 18:29:46 -0600

    18/28 tests pass

--- api/schemes/scheme-model.js
+++ api/schemes/scheme-model.js
@@ -5,24 +5,46 @@ function find() {
       .leftJoin('steps as st', 'sc.scheme_id', 'st.scheme_id')
       .select('sc.*')
       .count('st.step_id as number_of_steps')
-      .groupBy('sc.scheme_id')
+      .groupBy('sc.scheme_id');
 }
 
-function findById(scheme_id) {
+async function findById(scheme_id) {
+    const rows = await db('schemes as sc')
+      .leftJoin('steps as st', 'sc.scheme_id', 'st.scheme_id')
+      .where('sc.scheme_id', scheme_id)
+      .select('st.*', 'sc.scheme_name', 'sc.scheme_id')
+      .orderBy('st.step_number');
 
+      const result = {
+        scheme_id: rows[0].scheme_id,
+        scheme_name: rows[0].scheme_name,
+        steps: []
+      };
+
+      rows.forEach(row => {
+        if (row.step_id) {
+          result.steps.push({
+            step_id: row.step_id,
+            step_number: row.step_number,
+            instructions: row.instructions,
+          })
+        }
+      });
+
+      return result;
 }
 
 function findSteps(scheme_id) {
-   
+  return db('schemes as sc')
+    .leftJoin('steps as st', 'sc.scheme_id', 'st.scheme_id')
+    .select('st.step_id', 'st.step_number', 'instructions', 'sc.scheme_name')
+    .where('sc.scheme_id', scheme_id)
+    .orderBy('step_number')
 }
 
-function add(scheme) {
-  
-}
+function add(scheme) {}
 
-function addStep(scheme_id, step) {
-
-}
+function addStep(scheme_id, step) {}
 
 module.exports = {
     find,
