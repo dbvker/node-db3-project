710087df33a2f99b37def3b10f2860c1543be904
Dylan Baker <dylanmbaker@yahoo.com>
2022-02-16 18:10:05 -0600

    9/28 tests pass

--- api/schemes/scheme-middleware.js
+++ api/schemes/scheme-middleware.js
@@ -1,58 +1,41 @@
 const db = require('../../data/db-config');
 
-/*
-  If `scheme_id` does not exist in the database:
-
-  status 404
-  {
-    "message": "scheme with scheme_id <actual id> not found"
-  }
-*/
-
 const checkSchemeId = async (req, res, next) => {
-  const { scheme_id } = req.params
-  try {
-    const existing = await db('schemes')
-      .where('scheme_id', scheme_id)
-      .first();
+    const { scheme_id } = req.params;
+    try {
+        const existing = await db('schemes').where('scheme_id', scheme_id).first();
 
-      if (!existing) {
-        next({ status: 400, message: `scheme with scheme_id ${scheme_id} not found` })
-      } else {
-        next();
-      }
-  } catch (err) {
-    next(err);
-  }
-}
+        if (!existing) {
+            next({ status: 400, message: `scheme with scheme_id ${scheme_id} not found` });
+        } else {
+            next();
+        }
+    } catch (err) {
+        next(err);
+    }
+};
 
-/*
-  If `scheme_name` is missing, empty string or not a string:
-
-  status 400
-  {
-    "message": "invalid scheme_name"
-  }
-*/
 const validateScheme = (req, res, next) => {
+    const { scheme_name } = req.body;
+    if (scheme_name === undefined || typeof scheme_name !== 'string' || !scheme_name.trim()) {
+        next({ status: 400, message: 'invalid scheme_name' });
+    } else {
+        next();
+    }
+};
 
-}
+const validateStep = async (req, res, next) => {
+    const { instructions, step_number } = req.body;
 
-/*
-  If `instructions` is missing, empty string or not a string, or
-  if `step_number` is not a number or is smaller than one:
-
-  status 400
-  {
-    "message": "invalid step"
-  }
-*/
-const validateStep = (req, res, next) => {
-
-}
+    if (instructions === undefined || typeof instructions !== 'string' || !instructions.trim() || typeof step_number !== 'number' || step_number < 1) {
+        next({ status: 400, message: 'invalid step' });
+    } else {
+        next();
+    }
+};
 
 module.exports = {
-  checkSchemeId,
-  validateScheme,
-  validateStep,
-}
+    checkSchemeId,
+    validateScheme,
+    validateStep,
+};
